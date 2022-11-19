a0652590638fddc9f2ef948fafcf316daabcc31c
karim <karimgaberhd@gmail.com>
2022-11-19 17:35:12 +0200

    first commit

---
+++ Components/Experience.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+import ExperienceCard from './ExperienceCard'
+import { Experience } from '../typings'
+type Props = {
+  experiences:Experience[],
+}
+
+function Experiences({experiences}: Props) {
+  return (
+    <motion.div className='flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center h-screen'
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1.5}}
+    >
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3> 
+    
+         <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80'>
+          {experiences?.map((experience)=>(
+            <ExperienceCard key={experience?._id} experience={experience}/>
+          ))}
+         </div>
+    </motion.div>
+  )
+}
+
+export default Experiences
---
+++ Components/Hero.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import React from 'react'
+import { Cursor ,useTypewriter } from 'react-simple-typewriter'
+import { urlFor } from '../sanity'
+import { PageInfo } from '../typings'
+import BackgroundCircle from './BackgroundCircle'
+
+type Props = {
+  pageInfo:PageInfo,
+}
+
+function Hero({pageInfo}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+            `Hi, The Name Is ${pageInfo?.name}`,
+            "Guy-who-loves-Coffee.tsx",
+            "<ButLovesToCodeMore/>"
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    })
+  return (
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircle />
+        <img className='relative rounded-full h-32 w-32 mx-auto object-cover' src={urlFor(pageInfo?.heroImage).url()} alt='My Picture'/>
+        <div className='z-20'>
+            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>{pageInfo?.role}</h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#f7ab0a' />
+        </h1>
+        <div className='pt-5'>
+
+            <Link href="#About">
+              <button className='heroButton'>About</button>
+            </Link>
+            <Link href="#Experience">
+              <button className='heroButton'>Experience</button>
+            </Link>
+            <Link href="#Skills">
+              <button className='heroButton'>Skills</button>
+            </Link>
+            <Link href="#Projects">
+              <button className='heroButton'>Projects</button>
+            </Link>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+
+export default Hero
